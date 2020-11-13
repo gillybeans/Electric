60cf623cc7456411dee6987d9d5fda61eec9aefe
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2020-11-13 14:29:14 +0100

    added function js

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ function openForm() {
     document.getElementById("myForm").style.display = "none";
   }
 
+function openFormTwo() {
+    document.getElementById("myFormTwo").style.display = "block";
+  }
+  
+  function closeFormTwo() {
+    document.getElementById("myFormTwo").style.display = "none";
+  }
+
 
 
   
